8405051ebdd9270e8cbd05d611c489d4a845ebfa
Deathgun <32635968+koalapanda@users.noreply.github.com>
2017-11-13 21:14:04 +0700

    v 4

--- app/src/main/java/genius/koalapanda/MainFragment.java
+++ app/src/main/java/genius/koalapanda/MainFragment.java
@@ -1,7 +1,7 @@
 package genius.koalapanda;
 
 import android.app.Activity;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v13.app.FragmentPagerAdapter;
