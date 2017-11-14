00b30bddfeea9586e607a51f96d165bd36321706
NajmaZahra <najmazahra792@gmail.com>
2017-11-14 13:58:04 +0700

    g

--- app/src/main/java/genius/koalapanda/fragment/page444.java
+++ app/src/main/java/genius/koalapanda/fragment/page444.java
@@ -29,6 +29,6 @@ public class page444  extends Fragment {
         super.onActivityCreated(savedInstanceState);
         context = getActivity();
         pg1 = (ImageView) getView().findViewById(R.id.img444);
-        pg1.setImageResource(R.mipmap.a445);
+        pg1.setImageResource(R.drawable.a445);
     }
     }
